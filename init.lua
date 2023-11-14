354f027a524e66f277bad1439aca6a0bfaf80557
Pat Hacker <128792+phacker@users.noreply.github.com>
2023-11-14 08:34:25 -0800

    add init.lua, + neo-tree

---
+++ init.lua
@@ -0,0 +1,31 @@
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter = blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch = stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = false
+
+vim.keymap.set('n', '<space>w', '<cmd>write<cr>', {desc = 'Save'})
+
+require("lazy").setup('plugins')
+
